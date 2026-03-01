56bf589588847f84eb45b6de329216586e0431f4
Andreas Glaser <andreas.glaser@pm.me>
2026-03-01 17:11:13 +0800

    style: add PARAMETER_SENSOR_NAMES to devcode 6515 and 6544 for consistency with template

--- custom_components/dessmonitor/device_support/devcode_6515.py
+++ custom_components/dessmonitor/device_support/devcode_6515.py
@@ -68,6 +68,8 @@ SENSOR_TITLE_MAPPINGS: dict[str, str] = {
 
 VALUE_TRANSFORMATIONS: dict = {}
 
+PARAMETER_SENSOR_NAMES: set[str] = set()
+
 DEVCODE_CONFIG = {
     "device_info": DEVICE_INFO,
     "output_priority_mapping": OUTPUT_PRIORITY_MAPPING,
@@ -75,4 +77,5 @@ DEVCODE_CONFIG = {
     "operating_mode_mapping": OPERATING_MODE_MAPPING,
     "sensor_title_mappings": SENSOR_TITLE_MAPPINGS,
     "value_transformations": VALUE_TRANSFORMATIONS,
+    "parameter_sensor_names": PARAMETER_SENSOR_NAMES,
 }
