852105194ad7c37fb37d2f1f76b61333d6e25954
algrishina <agrishina@me.com>
2026-04-17 21:15:08 +0300

    feat: register devcode 2507 in device registry

--- custom_components/dessmonitor/device_support/device_registry.py
+++ custom_components/dessmonitor/device_support/device_registry.py
@@ -64,6 +64,10 @@ def _load_device_configurations() -> None:
 
         _register_devcode(2452, config_2452)
 
+        from .devcode_2507 import DEVCODE_CONFIG as config_2507
+
+        _register_devcode(2507, config_2507)
+
     except ImportError as err:
         _LOGGER.error("Failed to import device configuration: %s", err)
 
